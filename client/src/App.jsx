f92d550b1917541b5b375e72de0a5b570958c829
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-02-01 23:14:59 +0530

    private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Projects  from './pages/Projects'
 import Error from './pages/Error'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -18,7 +19,12 @@ const App = () => {
         <Route path='/about' element={<About/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+
+        {/* //private route */}
+        <Route element={<PrivateRoute/>}>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
+
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/*' element={<Error/>}/>
       </Routes>
