518527eca250b13cdfd79680d3575ec6d37b3cec
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-02-09 01:45:21 +0530

    added update post func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
 import AdminPrivateRoute from './components/AdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
+import UpdatePost from './pages/UpdatePost'
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
         {/* //private route Admin*/}
         <Route element={<AdminPrivateRoute/>}>
           <Route path='/create-post' element={<CreatePost/>}/>
+          <Route path='/update-post/:postId' element={<UpdatePost/>}/>
         </Route>
 
         <Route path='/projects' element={<Projects/>}/>
